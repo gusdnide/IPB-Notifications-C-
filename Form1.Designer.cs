2303f35add942a6dd7c236f2d1a8b594d166e3ab
Gabriel <gusdnide99@hotmail.com>
2016-09-01 14:36:28 -0300

    Arquivos
    Arquvos

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace FPS_GH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(988, 592);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 592);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FPS_GH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class FPSGH
+        {
+            public static string lPageInicial = "http://forum.fpsgh.org/";
+            public static string lNotificacoes = "http://forum.fpsgh.org/index.php?app=core&module=usercp&area=notificationlog";
+            public static string iNotificacoes = "ipsHasNotifications";
+            public static string lInbox = "http://forum.fpsgh.org/index.php?app=members&module=messaging";
+            public static string iInPage = "user_link";
+            public static string lLogout = "http://forum.fpsgh.org/index.php?app=core&module=global&section=login&do=logout";
+        }
+        NotifyIcon nIcon = new NotifyIcon();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            webBrowser1.Navigate(FPSGH.lPageInicial);
+            webBrowser1.ScriptErrorsSuppressed = true;
+            timer1.Start();
+            nIcon.Icon = this.Icon;
+            nIcon.Text = "FPSGH Notify";
+            nIcon.Visible = true;
+            nIcon.BalloonTipClicked += NIcon_BalloonTipClicked;
+            timer1.Start();
+        }
+
+        private void NIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(FPSGH.lNotificacoes);
+        }
+
+        bool Logando = true;
+        int UltimoNumNotificacoes = 0;
+        int UltimoNumMsg = 0;
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            webBrowser1.Navigate(FPSGH.lLogout);
+        }
+
+        HtmlElement BuscarNotfy()
+        {
+            HtmlElement Retorno = null;
+            foreach (HtmlElement e in webBrowser1.Document.GetElementById("notify_link").All)
+            {
+                if (e.GetAttribute("className") == FPSGH.iNotificacoes)
+                    Retorno = e;
+            }
+            return Retorno;
+        }
+        HtmlElement BuscarInbox()
+        {
+            HtmlElement Retorno = null;
+            foreach (HtmlElement e in webBrowser1.Document.GetElementById("inbox_link").All)
+            {
+                if (e.GetAttribute("className") == FPSGH.iNotificacoes)
+                    Retorno = e;
+            }
+            return Retorno;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!Logando)
+            {
+                if (webBrowser1.Url.ToString() == FPSGH.lPageInicial)
+                {
+                    if (webBrowser1.DocumentText.Contains(FPSGH.iInPage))
+                    {
+                        if (webBrowser1.DocumentText.Contains(FPSGH.iNotificacoes))
+                        {
+                            HtmlElement elNotfity = BuscarNotfy();
+                            HtmlElement elInbox = BuscarInbox();
+                            string MSG = "";
+                            if (elNotfity == null && elInbox == null)
+                            {
+                                webBrowser1.Refresh();
+                                return;
+                            }
+                            if (elNotfity != null)
+                            {
+                                int Notificacoes = int.Parse(elNotfity.InnerText.Replace(" ", ""));
+                                if (Notificacoes != UltimoNumNotificacoes)
+                                {
+                                    UltimoNumNotificacoes = Notificacoes;
+                                    MSG = "Você tem " + Notificacoes + " notificaçoes novas!";
+                                }
+                            }
+                            if(elInbox != null)
+                            {
+                                int Messages = int.Parse(elInbox.InnerHtml.Replace(" ", ""));
+                                if(Messages != UltimoNumMsg)
+                                {
+                                    UltimoNumMsg = Messages;
+                                    MSG += "\n Você tem " + Messages + " Mensagens novas!";
+                                }
+                            }
+                            if(MSG != "")
+                            nIcon.ShowBalloonTip(2000, "Novas Notificaçoes", MSG,  ToolTipIcon.Info);
+                        }
+                        else
+                        {
+                            webBrowser1.Refresh();
+                        }
+                    }
+                    else
+                    {
+                        Logando = true;
+                        nIcon.Visible = false;
+                        this.Show();
+                        webBrowser1.Navigate(FPSGH.lPageInicial);
+                        MessageBox.Show("Você não está logado!");
+                    }
+                }
+                else
+                {
+                    webBrowser1.Navigate(FPSGH.lPageInicial);
+                }
+                webBrowser1.Refresh();
+            }
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.DocumentText.Contains(FPSGH.iInPage)) { this.Hide(); nIcon.Visible = true; Logando = false; }
+        }
+    }
+}
